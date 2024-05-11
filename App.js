e1d4e65004be34fad3f6325f131f6f0d829ee90c
Santhoshkumar1032 <108716331+Santhoshkumar1032@users.noreply.github.com>
2024-05-11 22:30:54 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Chatbot from './Chatbot';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+       <Chatbot/>
+    </div>
+  );
+}
+
+export default App;
